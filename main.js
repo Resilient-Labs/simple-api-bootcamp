bb6c6d2c420302f3ec149c714c1a73245422b614
Faisal Rashed <f.f0001989@gmail.com>
2019-07-09 21:25:28 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//this app is for images and videoS from NASA
+//NASA API key : qodkuyKgxvT7QDDMpOtZQ83g57OKLgQyqAVTjqxs
+//https://api.nasa.gov/api.html#Images
+
+document.getElementById("searchMe").addEventListener("click", doSearch);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function doSearch() {
+  fetch("https://images-api.nasa.gov/search?q=moon")
+    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+    .then(response => {
+      const random = getRandomInt(0, 99);
+      document.getElementById("imageS").src = response.collection.items[random].links[0].href;
+    })
+    .catch(err => {
+      alert("sorry, there are no results for your search")
+    });
+}
