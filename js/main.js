f6314014eb30689fa3b38bb495e0a87a2455ac1a
kim-flores <kimflores.k@gmail.com>
2020-09-06 19:58:02 -0400

    added js comments

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-let button = document.querySelector("button");
+let button = document.querySelector("button"); //selects button
 
 // function catFacts () {
     button.addEventListener("click", ()=>{
-        fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=1`)
-        .then(res => res.json()) 
+        fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=1`) //fetch call
+        .then(res => res.json()) //parse json
         .then(data => {
-        console.log(data.text)
-       document.getElementById("facts").textContent = data.text;
+        console.log(data.text) 
+       document.getElementById("facts").textContent = data.text; //displays data into p tag
      });
     });
 
 
 
-    
+
     //  button.addEventListener("click", (event) =>{
     //     event.preventDefault()
     //   })
@@ -32,8 +32,7 @@ let button = document.querySelector("button");
     //        document.getElementById("facts").textContent = data.text;
     //      });
         
-    //      button.addEventListener("click", () =>{
-    //         event.preventDefault()
+    //      button.addEventListener("click", fetchFact)
     //       })
     //       .catch(err => console.log(err))
     //     } 
