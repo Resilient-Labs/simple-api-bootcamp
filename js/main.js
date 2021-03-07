de57c77608483c949b0d0da8900e97c6a7b1e63d
Davida Garner <davidac.garner@gmail.com>
2021-03-07 01:56:33 -0500

    answer to simple api push two

--- js/main.js
+++ js/main.js
@@ -1,34 +1,22 @@
 
-
 const btn = document.getElementById('button');
-const images = document.getElementById('dogPic')
-const section = document.querySelector('.smaller')
-// let count = 0
+const images = document.getElementById('catPic')
+const section = document.querySelector('.showPic')
 
 
-btn.addEventListener('click', getRandomDog);
+btn.addEventListener('click', getRandomCat);
 
 
-function getRandomDog() {
-    // let apiKey = "T0jznlsubW13V7xCOLsFgaXlMTwg8HId"
-    // fetch(`https://api.giphy.com/v1/gifs/search?q=${text}&api_key=${apiKey}&limit=1`)
-    // .then(res => res.json()) 
-    // .then(giffy => {
-    //   document.querySelector("img").src = giffy.data[0].images.original.url
-    // })    
-    // count++
-
-	fetch('https://random.dog/woof.json')
+function getRandomCat() {
+	fetch('https://aws.random.cat/meow')
 		.then(res => res.json())
 		.then(data => {
-			if(data.url.includes('.mp4')) {
-				getRandomDog();
-			}
-			else {
-				section.innerHTML = `<img src=${data.url} alt="images dogs " />`;
-			}
+            section.innerHTML = `<img src=${data.file} alt="cat" />`
+        // .catch(err =>  {
+        //     console.log(`error ${err}`)
+        // })
+
    
 	});
 }
-
 
