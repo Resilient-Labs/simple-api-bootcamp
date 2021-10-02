a2a32cb649d1e14a29a84eaa01175d8eda0a0bb9
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-10-02 18:31:30 -0400

    Accepts word input and outputs the definitions

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.querySelector('#submit').addEventListener('click', getInfo)
+
+const displayDefinition = document.querySelector('#allDefinitions')
+
+function getInfo() {
+  let word = document.querySelector('#word').value
+  let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=___________`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      document.querySelector('#wordInput').innerText = data[0].meta.id
+      for (let i = 0; i < data[0].shortdef.length; i++) {
+        let displayDefinitions = data[0].shortdef[i]
+        const li = document.createElement('li')
+
+        li.innerText = displayDefinitions
+        document.querySelector('#allDefinitions').appendChild(li)
+      }
+    })
+    .catch(err => {
+      document.querySelector('h2').hidden = true
+      displayDefinition.innerText = "Oh no, there seems to be an error"
+      console.log(`error${err}`)
+    })
+}
