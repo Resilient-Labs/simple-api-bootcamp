47ec2027804ccde8979d14cf20cfdd8b955d7d57
roxanalemus <roxanalemusdev@gmail.com>
2021-10-02 23:13:51 -0400

    initial submission

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Goal:  Retrieve a random cat fact when button is clicked.
+document.querySelector('button').addEventListener('click', getFact)
+function getFact(){
+    const url = "https://meowfacts.herokuapp.com/"
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('h2').innerHTML = data.data
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+}
+
+
+/*
+Contributors:
+
+Alexa
+Miriam
+Shannon
+David
+Roxana
+*/
