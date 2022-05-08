6e09008801f17139ed86100db577431cd88ea391
Alexandra Harley <aharleydev@gmail.com>
2022-05-07 23:43:51 -0400

    apis work, added random pokemon generator, search by id instead of name. Number range provided for the user. error messages just in case.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 document.querySelector('.submitBtn').addEventListener('click', runTime)
+document.querySelector('.randomBtn').addEventListener('click', randomPoke)
+
 
 function runTime() {
-    const pokemon = document.querySelector('.pokemonName').value
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    const pokemon = document.querySelector('.pokemonID').value
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
 
     fetch(url)
         .then(res => res.json()) // doesn't check for a JSON, it actually takes the response and parses it to a JSON object. Often times a fetch call will return an html, and that would be the response. res.json will parse the info, parse = translate response to a JSON
         .then(data => {
             console.log(data)
-            document.querySelector('.name').innerText = data.name
+            document.querySelector('.name').innerText = "Name: " + data.name
+            document.querySelector('.number').innerText = "ID#: " + data.id
             document.querySelector('.pokePic').src = data.sprites.front_default
         })
         .catch(err => {
             console.log(`error ${err}`)
-            document.querySelector('.name').innerText = 'Are you sure that\'s a pokemon? Check your spelling and try again...'
+            document.querySelector('.name').innerText = 'Are you sure that\'s a pokemon id # that is less than or equal to 898? Check your spelling and try again...'
         })
 
+}
+
+function randomPoke() {
+    let number = Math.floor(Math.random() * 899)
+    let url = `https://pokeapi.co/api/v2/pokemon/${number}`
+
+    fetch(url)
+        .then(res => res.json()) // doesn't check for a JSON, it actually takes the response and parses it to a JSON object. Often times a fetch call will return an html, and that would be the response. res.json will parse the info, parse = translate response to a JSON
+        .then(data => {
+            console.log(data)
+            document.querySelector('.name').innerText = "Name: " + data.name
+            document.querySelector('.number').innerText = "ID#: " + data.id
+            document.querySelector('.pokePic').src = data.sprites.front_default
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+            document.querySelector('.name').innerText = 'Are you sure that\'s a pokemon id # that is less than or equal to 898? Check your spelling and try again...'
+        })
 }
